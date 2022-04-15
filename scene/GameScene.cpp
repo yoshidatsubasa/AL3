7a6088ddef0e69a990cb9667b4f83b1edb8778cb
tsubasayoshida <k021g1136@g.neec.ac.jp>
2022-04-15 13:10:48 +0900

    02_01＿確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -23,27 +23,41 @@ void GameScene::Initialize() {
 	worldTransform_.Initialize();
 	viewProjection_.Initialize();
 	soundDataHandle_ = audio_->LoadWave("se_sad03.wav");
-	audio_->PlayWave(soundDataHandle_);
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
+	/*audio_->PlayWave(soundDataHandle_);
+	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);*/
+
+	// X Y Z方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
+	
+	// X Y Z軸周りの回転角を設定
+	worldTransform_.rotation_ = {XM_PI/4.0f,XM_PI/ 4.0f, 0.0f};
+	
+	// X Y Z軸周りの平行移動を設定
+	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+
 	
 }
 
 void GameScene::Update() { XMFLOAT2 position = sprite_->GetPosition(); 
     position.x += 2.0f;
 	position.y += 1.0f;
-	//debugText_->Print("Kaizokuou ni oreha naru", 50, 50, 1.0f);
+	debugText_->Print("translation:(10.000000,10.000000,10.000000)", 50, 50, 1.0f);
+	debugText_->Print("rotation:(0.785398,0.785398,0.000000)", 50, 70, 1.0f);
+	debugText_->Print("scale:(5.000000,5.000000,5.000000)", 50, 90, 1.0f);
 	debugText_->SetPos(50,70);
 	//debugText_->Printf("year:%d", 2001);
 	sprite_->SetPosition(position);
 	if (input_->TriggerKey(DIK_SPACE)) {
 		audio_->StopWave(voiceHandle_);
 	}
-	//変数の値をイクリメント
-	value_++;
-	//値を含んだ文字列
-	std::string strDebug = std::string("Value:") + std::to_string(value_);
-	//デバッグテキストの表示
-	debugText_->Print(strDebug, 50, 60, 1.0f);
+	////変数の値をイクリメント
+	//value_++;
+	////値を含んだ文字列
+	//std::string strDebug = std::string("Value:") + std::to_string(value_);
+	////デバッグテキストの表示
+	//debugText_->Print(strDebug, 50, 60, 1.0f);
 }
 
 void GameScene::Draw() {
@@ -61,19 +75,19 @@ void GameScene::Draw() {
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
-#pragma region 背景スプライト描画
-	// 背景スプライト描画前処理
-	Sprite::PreDraw(commandList);
-
-	/// <summary>
-	/// ここに背景スプライトの描画処理を追加できる
-	/// </summary>
-	
-	sprite_->Draw();
-	// スプライト描画後処理
-	Sprite::PostDraw();
-	// 深度バッファクリア
-	dxCommon_->ClearDepthBuffer();
+//#pragma region 背景スプライト描画
+//	// 背景スプライト描画前処理
+//	Sprite::PreDraw(commandList);
+//
+//	/// <summary>
+//	/// ここに背景スプライトの描画処理を追加できる
+//	/// </summary>
+//	
+//	sprite_->Draw();
+//	// スプライト描画後処理
+//	Sprite::PostDraw();
+//	// 深度バッファクリア
+//	dxCommon_->ClearDepthBuffer();
 #pragma endregion
 
 //#pragma region 3Dオブジェクト描画
